2b400ba74d58e5baba312a27a096b1e03cd2f3b3
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-12 14:15:20 +0200

    with url

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import * as config from '../config';
+import * as config from "../config";
+
+const languagesUrl = `${config.backendUrl()}/languages`; 
 
 export const PageWelcome = () => {
 	const [languages, setLanguages] = useState<string[]>([]);
 
 	useEffect(() => {
 		(async () => {
-			const response = await fetch(`${config.backendUrl()}/languages`);
+			const response = await fetch(languagesUrl);
 			const _languages: string[] = await response.json();
 			setLanguages(_languages);
 		})();
@@ -14,7 +16,12 @@ export const PageWelcome = () => {
 
 	return (
 		<>
-			<p>There are there {languages.length} languages.</p>
+			<p>There are there {languages.length} languages, being loaded from <a target="_blank" className="underline" href={languagesUrl}>{languagesUrl}</a>.</p>
+			<ul className="list-disc ml-4">
+				{languages.map((language, index) => {
+					return <li key={index}>{language}</li>;
+				})}
+			</ul>
 		</>
 	);
 };
