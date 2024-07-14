549cb3e8a86b7b89222cb6339f98c8956cd823ed
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-14 14:12:20 +0200

    first component

---
+++ src/components/ExampleDataFromCode.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { ExampleArea } from "./ExampleArea";
+import * as config from "../config";
+
+const languagesUrl = `${config.backendUrl()}/languages`;
+
+export const ExampleDataFromCode = () => {
+	const [languages, setLanguages] = useState<string[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(languagesUrl);
+			const _languages: string[] = await response.json();
+			setLanguages(_languages);
+		})();
+	}, []);
+
+	return (
+			<ExampleArea title="Data from code">
+				<p className="mb-2">
+					These are {languages.length} languages being loaded from:{" "}
+					<a
+						target="_blank"
+						className="underline"
+						href={languagesUrl}
+					>
+						{languagesUrl}
+					</a>
+					.
+				</p>
+				<ul className="list-disc ml-4">
+					{languages.map((language, index) => {
+						return <li key={index}>{language}</li>;
+					})}
+				</ul>
+			</ExampleArea>
+	)
+}
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,22 +1,12 @@
 import { useEffect, useState } from "react";
 import * as config from "../config";
 import { Skill } from "../types";
-import { Example, ExampleArea } from "../components/ExampleArea";
+import { ExampleDataFromCode } from "../components/ExampleDataFromCode";
 
-const languagesUrl = `${config.backendUrl()}/languages`;
 const skillsUrl = `${config.backendUrl()}/skills`;
 
 export const PageWelcome = () => {
-	const [languages, setLanguages] = useState<string[]>([]);
 	const [skills, setSkills] = useState<Skill[]>([]);
-
-	useEffect(() => {
-		(async () => {
-			const response = await fetch(languagesUrl);
-			const _languages: string[] = await response.json();
-			setLanguages(_languages);
-		})();
-	}, []);
 
 	useEffect(() => {
 		(async () => {
@@ -28,27 +18,7 @@ export const PageWelcome = () => {
 
 	return (
 		<>
-			<ExampleArea title="Data from code">
-				<p className="mb-2">
-					These are {languages.length} languages being loaded from:{" "}
-					<a
-						target="_blank"
-						className="underline"
-						href={languagesUrl}
-					>
-						{languagesUrl}
-					</a>
-					.
-				</p>
-				<ul className="list-disc ml-4">
-					{languages.map((language, index) => {
-						return <li key={index}>{language}</li>;
-					})}
-				</ul>
-			</ExampleArea>
-			<div>
-				<p></p>
-			</div>
+			<ExampleDataFromCode />
 		</>
 	);
 };
