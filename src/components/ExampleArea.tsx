06de891c20e07f006a55ebaf9d70fed8025ea7f5
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-14 14:06:58 +0200

    new style

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -5,10 +5,10 @@ interface IProps {
 export const ExampleArea = ({ title, children }: IProps) => {
 	return (
 		<div className="mb-3 w-full md:w-[40rem]">
-			<div className="bg-slate-700 text-slate-100 px-4 py-3 rounded-t">
+			<div className="bg-slate-700 text-yellow-400 px-4 py-3 rounded-t">
 				<p>{title}</p>
 			</div>
-			<div className="bg-slate-600 text-slate-100 px-4 py-3 rounded-b mb-3 font-mono text-xs">
+			<div className="bg-slate-200 text-slate-900 px-4 py-3 rounded-b mb-3 font-mono text-xs">
 				{children}
 			</div>
 		</div>
--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -3,8 +3,20 @@ import * as config from "../config";
 export const PageAbout = () => {
 	return (
 		<>
+			<p>
+				This site is a demonstration of a Go Backend working with a
+				React frontend.
+			</p>
 			<p>This site gets its information from the following URL:</p>
-			<p><a href={config.backendUrl()} className="underline" target="_blank">{config.backendUrl()}</a></p>
+			<p>
+				<a
+					href={config.backendUrl()}
+					className="underline"
+					target="_blank"
+				>
+					{config.backendUrl()}
+				</a>
+			</p>
 		</>
 	);
 };
