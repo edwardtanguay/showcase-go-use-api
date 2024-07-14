74b9c1b0aa1b372b4ba9eccb7aea262277f4b293
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-14 13:48:25 +0200

    example style

--- src/components/ExampleArea.tsx
+++ src/components/ExampleArea.tsx
@@ -1,7 +1,12 @@
 export const ExampleArea = () => {
 	return (
-		<div className="bg-slate-700 text-slate-100 px-2 py-1 rounded mb-3">
+		<div className="mb-3 w-full md:w-[40rem]">
+		<div className="bg-slate-700 text-slate-100 px-2 py-1 rounded-t">
 			<p>example</p>
+		</div>
+		<div className="bg-slate-600 text-slate-100 px-2 py-1 rounded-b mb-3">
+			<p>example</p>
+		</div>
 		</div>
 	);
 };
